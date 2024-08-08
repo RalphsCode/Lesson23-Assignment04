65d254fb2ecef71e2a29800f324a77ab8125fac8
Ralph's Code <RalphsCode@gmail.com>
2024-08-08 12:32:37 -0700

    Added SQL Alchemy hooks

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_sqlalchemy import SQLAlchemy
+from models import db, connect_db
 
 
 # app configuration files
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "RalphsCode123"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Ponderosa@localhost/flask_feedback'
 
 debug = DebugToolbarExtension(app)
+
+with app.app_context():
+    connect_db(app)
+    db.create_all()
 
 @app.route('/')
 def home():
