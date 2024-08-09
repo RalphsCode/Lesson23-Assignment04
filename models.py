d74f07a183169ea6b374d727636fb329d95f3675
Ralph's Code <RalphsCode@gmail.com>
2024-08-09 10:43:10 -0700

    Created Model to create the users table in the flask_feedback database, added bootstrap link in the head file of the base.html.

--- models.py
+++ models.py
@@ -7,3 +7,21 @@ def connect_db(app):
     db.init_app(app)
 
 # models
+
+class User(db.Model):
+    """User table to track users and their password hash"""
+
+    __tablename__ = "users"
+
+    username = db.Column(db.String(20), primary_key= True)
+    
+    password = db.Column(db.Text,
+                     nullable=False)
+
+    email = db.Column(db.String(50), nullable=False)
+
+    first_name = db.Column(db.String(30), nullable=False)
+
+    last_name = db.Column(db.String(30), nullable=False)
+
+    
