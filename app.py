228d216849f05254634c155f08031693141017b4
Ralph's Code <RalphsCode@gmail.com>
2024-08-10 16:05:38 -0700

    Added the log in form, fixed a bug in the register functionality.

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
 from models import db, connect_db, User
-from forms import Register
+from forms import Register, Login
 
 # app configuration files
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "RalphsCode123"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Ponderosa@localhost/flask_feedback'
+app.config['SECRET_KEY'] = 'RalphsCode123'
 
 debug = DebugToolbarExtension(app)
 
@@ -33,13 +34,31 @@ def register():
         flash("Form Submitted Successfully.")
         username = form.username.data
         password = form.password.data
+        # Hash the password
+        password_hashed = hash(password)
+        # password_utf = password_hashed.encode('utf-8')
         email = form.email.data
         first_name = form.first_name.data
         last_name = form.last_name.data
 
-        new_user = User(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+        # create a new_user object
+        new_user = User(username=username, password=password_hashed, email=email, first_name=first_name, last_name=last_name)
+        # insert the new_user object into the database
         db.session.add(new_user)
         db.session.commit()
-        return redirect('/home')
+
+        # set the user session variable 
+        session['username'] = [new_user.username]
+        return redirect('/secret')
 
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = Login()
+    return render_template('login.html', form=form)
+
+
+@app.route('/secret')
+def secret():
+    return "You made it!"
--- forms.py
+++ forms.py
@@ -9,3 +9,8 @@ class Register(FlaskForm):
     email = StringField("Email", validators=[InputRequired()])
     first_name = StringField("First Name", validators=[InputRequired()])
     last_name = StringField("Last Name", validators=[InputRequired()])
+
+class Login(FlaskForm):
+    """User Log In form"""
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])    
